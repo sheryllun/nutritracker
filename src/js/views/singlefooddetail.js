5719601706242ae59975aa989dd5f09c06f1d164
sheryllun <sheryl.v.lun@gmail.com>
2016-03-15 23:37:49 -0400

    Add validation for servings input, refactor single food detail view render()

--- src/js/views/singlefooddetail.js
+++ src/js/views/singlefooddetail.js
@@ -38,15 +38,15 @@ app.SingleFoodDescriptionView = Backbone.View.extend({
         serveQty: food.nf_serving_size_qty,
         serveUnit: food.nf_serving_size_unit,
         calories: food.nf_calories,
-        calcium: food.nf_calcium_dv = (food.nf_calcium_dv === null) ? 0 : food.nf_calcium_dv,
-        fiber: food.nf_dietary_fiber = (food.nf_dietary_fiber === null) ? 0 : food.nf_dietary_fiber,
+        calcium: (food.nf_calcium_dv === null) ? 0 : food.nf_calcium_dv,
+        fiber: (food.nf_dietary_fiber === null) ? 0 : food.nf_dietary_fiber,
         protein: food.nf_protein,
-        sodium: food.nf_sodium,
-        sugar: food.nf_sugars = (food.nf_sugars === null) ? 0 : food.nf_sugars,
+        sodium: (food.nf_sodium === null) ? 0 : food.nf_sodium,
+        sugar: (food.nf_sugars === null) ? 0 : food.nf_sugars,
         fat: food.nf_total_fat,
         carbs: food.nf_total_carbohydrate,
-        vitA: food.nf_vitamin_a_dv = (food.nf_vitamin_a_dv === null) ? 0 : food.nf_vitamin_a_dv,
-        vitC: food.nf_vitamin_c_dv = (food.nf_vitamin_c_dv === null) ? 0 : food.nf_vitamin_c_dv
+        vitA: (food.nf_vitamin_a_dv === null) ? 0 : food.nf_vitamin_a_dv,
+        vitC: (food.nf_vitamin_c_dv === null) ? 0 : food.nf_vitamin_c_dv
       };
 
       this.$el.html(this.foodDescriptionTemplate({
@@ -70,38 +70,42 @@ app.SingleFoodDescriptionView = Backbone.View.extend({
   },
 
   addFood: function(e) {
-    var servings = parseInt($('#servings').val());
-    if(!servings) { return; }
+    $('.error').remove();
+    var servings = $('#servings').val();
+    if(!servings || !servings.match(/^[+]?\d+([.]\d+)?$/)) { 
+      $('.serving-input').append('<p class="error">Please enter a positive number</p>');
+      return; 
+    } else {
+      var foodName = $('.item-name').text();
+      var calories = parseInt($('.calories').text());
+      var fiber = parseInt($('.fiber').text());
+      var protein = parseInt($('.protein').text());
+      var sodium = parseInt($('.sodium').text());
+      var sugar = parseInt($('.sugar').text());
+      var carbs = parseInt($('.carbs').text());
+      var fat = parseInt($('.fat').text());
+      var calcium = parseInt($('.calcium').text());
+      var vita = parseInt($('.vita').text());
+      var vitc = parseInt($('.vitc').text());
 
-    var foodName = $('.item-name').text();
-    var calories = parseInt($('.calories').text());
-    var fiber = parseInt($('.fiber').text());
-    var protein = parseInt($('.protein').text());
-    var sodium = parseInt($('.sodium').text());
-    var sugar = parseInt($('.sugar').text());
-    var carbs = parseInt($('.carbs').text());
-    var fat = parseInt($('.fat').text());
-    var calcium = parseInt($('.calcium').text());
-    var vita = parseInt($('.vita').text());
-    var vitc = parseInt($('.vitc').text());
-
-    app.foodList.create({ 
-      name: foodName, 
-      servings: servings, 
-      calories: calories,
-      fiber: fiber,
-      protein: protein,
-      sodium: sodium,
-      sugar: sugar,
-      carbs: carbs,
-      fat: fat,
-      calcium: calcium,
-      vitA: vita,
-      vitC: vitc,
-      totalCals: calories * servings
-    });
-    
-    $('#servings').val('');
-    location.href = '#mylist';
+      app.foodList.create({ 
+        name: foodName, 
+        servings: servings, 
+        calories: calories,
+        fiber: fiber,
+        protein: protein,
+        sodium: sodium,
+        sugar: sugar,
+        carbs: carbs,
+        fat: fat,
+        calcium: calcium,
+        vitA: vita,
+        vitC: vitc,
+        totalCals: calories * servings
+      });
+      
+      $('#servings').val('');
+      location.href = '#mylist';
+    }
   }
 });
